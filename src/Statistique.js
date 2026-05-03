3fe98d1f7e6e56e15b5c817aa89f2e9139e18c91
AissatouAM <jopmarieaissatou@gmail.com>
2026-05-03 12:45:22 +0000

    Lab 2 : composants Statistiques LigneBus et ListeLignes

---
+++ src/Statistique.js
@@ -0,0 +1,27 @@
+import './Statistique.css'
+
+function Statistique1(){
+  return (
+    <div className='statistique'>
+      <p>10 lignes</p>
+    </div>
+  );
+}
+
+function Statistique2(){
+  return (
+    <div className='statistique'>
+      <p>110 arrets</p>
+    </div>
+  );
+}
+
+function Statistique3(){
+  return (
+    <div className='statistique'>
+      <p>18 lignes</p>
+    </div>
+  );
+}
+
+export { Statistique1, Statistique2, Statistique3 };
